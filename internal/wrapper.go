e1f9556c131015e4c90b21510acb59768ac6529a
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-09-15 23:05:42 +0200

    preset: no longer need PresetWrapper to fallback to defaults

--- internal/wrapper.go
+++ internal/wrapper.go
@@ -10,22 +10,6 @@ func WrapPreset(preset govendor.Preset) *PresetWrapper {
 	return &PresetWrapper{preset}
 }
 
-func (w *PresetWrapper) GetSpecFilename() string {
-	if w.Preset == nil {
-		return govendor.SPEC_FILENAME
-	} else {
-		return w.Preset.GetSpecFilename()
-	}
-}
-
-func (w *PresetWrapper) GetSpecLockFilename() string {
-	if w.Preset == nil {
-		return govendor.SPEC_FILENAME
-	} else {
-		return w.Preset.GetSpecLockFilename()
-	}
-}
-
 func (w *PresetWrapper) LoadSpec() (*govendor.Spec, error) {
 	return govendor.LoadSpec(w)
 }
