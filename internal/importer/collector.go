40bd1728cf3dc7e3781c05971eb5d7a6498c8856
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-10-02 21:43:51 +0200

    internal/importer: use targetCollector to copy files
    targetCollector executes a WalkDir on the repository, and gathers
    the selected paths into `[]target`, the `target` can be individually
    copied.

---
+++ internal/importer/collector.go
@@ -0,0 +1,75 @@
+package importer
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/alevinval/vendor-go/pkg/log"
+)
+
+type targetCollector struct {
+	targets []target
+}
+
+type target struct {
+	src         string
+	dst         string
+	srcRelative string
+}
+
+func (tc *targetCollector) add(t target) {
+	tc.targets = append(tc.targets, t)
+}
+
+func (tc *targetCollector) copyAll() error {
+	for _, target := range tc.targets {
+		err := target.copy()
+		if err != nil {
+			return fmt.Errorf("cannot copyAll: %w", err)
+		}
+	}
+	return nil
+}
+
+func (t *target) copy() error {
+	log.S().Debugf("  ..%s -> %s", t.srcRelative, t.dst)
+
+	dstDir := filepath.Dir(t.dst)
+	err := os.MkdirAll(dstDir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("cannot create target path %q: %w", dstDir, err)
+	}
+	err = copyFile(t.src, t.dst)
+	if err != nil {
+		return fmt.Errorf("cannot import: %w", err)
+	}
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("cannot open %q: %w", src, err)
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("cannot create %q: %w", dst, err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return fmt.Errorf("cannot copy %q => %q: %w", src, dst, err)
+	}
+
+	err = out.Close()
+	if err != nil {
+		return fmt.Errorf("cannot close %q: %w", dst, err)
+	}
+
+	return nil
+}
