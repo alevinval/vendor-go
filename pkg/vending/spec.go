8d61c033ed5f47d97be3cdd5c99aff895402fc33
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2024-04-10 12:36:20 +0200

    release: version bump

--- pkg/vending/spec.go
+++ pkg/vending/spec.go
@@ -11,7 +11,7 @@ import (
 )
 
 // VERSION of the current tool
-const VERSION = "v0.4.8"
+const VERSION = "v0.4.9"
 
 // Spec holds relevant information related to the specification of what
 // versions need to be fetched when updating dependencies.
